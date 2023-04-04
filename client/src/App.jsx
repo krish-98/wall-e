e140ddc35ac9c1a9467a766f4797f1480196ee76
krish-98 <muralikrishnanm98@gmail.com>
2023-04-04 19:47:35 +0530

    Initial setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="bg-black h-screen text-center ">
+      <h1 className="text-white text-3xl font-bold animate-pulse">
+        Hello, Vite!
+      </h1>
+    </div>
+  )
+}
+export default App
